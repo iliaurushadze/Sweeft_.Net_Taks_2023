078531bc36066437262c7911ba4e497dd3f98464
iliaurushadze8 <iliaurushadze24@gmail.com>
2023-09-28 14:46:10 +0400

    Added Task 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the sequence - ");
+            string sequence = Console.ReadLine();
+            Console.WriteLine(IsProperly(sequence));
+        }
+
+        static bool IsProperly(string sequence)
+        {
+            Stack<Char> stack = new Stack<Char>();
+            foreach (var c in sequence)
+            {
+                if (c == '(')
+                    stack.Push(')');
+                else if (stack.Count == 0 || stack.Pop() != c)
+                    return false;
+            }
+            return (stack.Count == 0);
+        }
+    }
+}
