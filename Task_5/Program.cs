74b57ef51a0a310922af3509194ec07bd9310a32
iliaurushadze8 <iliaurushadze24@gmail.com>
2023-09-28 14:53:01 +0400

    Added Task 5

---
+++ Task_5/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number of stairs - ");
+            int stairCount = int.Parse(Console.ReadLine());
+            Console.WriteLine("Number of variants - " + CountVariants(stairCount));
+        }
+
+        static int CountVariants(int stairCount)
+        {
+            int[] dp = new int[stairCount + 1];
+            dp[0] = 1;
+            dp[1] = 1;
+
+            for (int i = 2; i <= stairCount; i++)
+            {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+
+            return dp[stairCount];
+        }
+    }
+}
