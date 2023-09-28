f3cc9447723d782ee13bb4c884f0f9f175f1d8b2
iliaurushadze8 <iliaurushadze24@gmail.com>
2023-09-28 14:29:08 +0400

    Add project files.

---
+++ Task_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the word - ");
+            string text = Console.ReadLine();
+            Console.WriteLine(IsPalindrome(text));
+        }
+
+        public static bool IsPalindrome(string text)
+        {
+            int start = 0;
+            int last = text.Length - 1;
+            while (start <= last)
+            {
+                char currFirst = text[start];
+                char currLast = text[last];
+                if (!Char.IsLetter(currFirst) && !Char.IsDigit(currFirst))
+                {
+                    start++;
+                }
+                else if (!Char.IsLetter(currLast) && !Char.IsDigit(currLast))
+                {
+                    last--;
+                }
+                else
+                {
+                    if (Char.ToLower(currFirst) != Char.ToLower(currLast))
+                    {
+                        return false;
+                    }
+                    start++;
+                    last--;
+                }
+            }
+            return true;
+        }
+    }
+}
